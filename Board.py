156790cdc3c8511e3478ba6708f56627f3fb109c
Riley Bridges <rlybrdgs@gmail.com>
2020-04-07 00:29:57 -0500

    Added display function that doesn't work

--- Board.py
+++ Board.py
@@ -42,4 +42,36 @@ class Board:
             s += "]"
         s += "]"
         return s
-    
+
+    def display(self, x_offset, y_offset, board_height, board_width, margin_percent):
+        
+        # draw horizontal lines
+        margin = board_height * (margin_percent / 100)
+        horizontal_line_spacing = (board_height - (2*margin)) / (self.rows)
+        for i in range(1, self.rows - 1):
+            line(x_offset + margin, y_offset + (i * horizontal_line_spacing), x_offset + (board_width - margin), y_offset + (i * horizontal_line_spacing))
+        
+        # draw vertical lines
+        margin = board_width * margin_percent
+        vertical_line_spacing = (board_width - (2*margin)) / (self.cols)
+        for i in range(1, self.cols - 1):
+            line(x_offset + (i*vertical_line_spacing), y_offset + margin, x_offset + (i*vertical_line_spacing), y_offset + (board_height-margin))
+        
+        # base case
+        if self.depth == 0:
+            textAlign(CENTER, CENTER)
+            textSize(board_height/self.rows)
+            for i, row in enumerate(self.spaces):
+                for j, space in enumerate(row):
+                    x = x_offset + (j * vertical_line_spacing) + (vertical_line_spacing / 2)
+                    y = y_offset + (i * horizontal_line_spacing) + (horizontal_line_spacing / 2)
+                    text(space, x, y)
+        
+        else:
+            for i, row in enumerate(self.spaces):
+                for j, space in enumerate(row):
+                    next_x_offset = x_offset + (board_width / self.cols)
+                    next_y_offset = y_offset + (board_height / self.rows)
+                    next_board_width = board_width / self.cols
+                    next_board_height = board_height / self.rows
+                    space.display(next_x_offset, next_y_offset, next_board_height, next_board_width, margin_percent)
--- ultimate_tictactoe.pyde
+++ ultimate_tictactoe.pyde
@@ -1,17 +1,22 @@
 from Board import Board
 
-def make_board(board, final_depth):
-    print(board.get_depth())
-    if board.get_depth() == final_depth:
-        print("base case")
+def make_board(board):
+    if board.get_depth() == 0:
         board.make_base_board()
     
     else:
         for i in range(board.get_rows()):
             for j in range(board.get_cols()):
-                 board.set_space(i, j, Board(depth = board.get_depth() + 1))
-                 make_board(board.get_space(i, j), final_depth)
+                 board.set_space(i, j, Board(depth = board.get_depth() - 1))
+                 make_board(board.get_space(i, j))
         
-board = Board(depth = 0)
-make_board(board, 0)
-print(board)
+def setup():
+    size(200, 200)
+    background(255)
+    fill(0)
+    stroke(10)
+    board = Board(depth = 2)
+    make_board(board)
+    #print(board)
+    
+    board.display(0, 0, height, width, 10)
